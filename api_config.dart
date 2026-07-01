c065459d06877a24b16c89142661d69919a93851
kumarravindra35887 <kumarravindra35887@gmail.com>
2026-07-01 11:09:37 +0530

    Add ApiConfig class with base URLs

---
+++ api_config.dart
@@ -0,0 +1,5 @@
+class ApiConfig {
+  static const String baseUrl = 'https://vercel.app';
+  static const String loginUrl = '$baseUrl/auth/login';
+  static const String uploadQuestionsUrl = '$baseUrl/admin/portal/upload-and-parse-questions';
+}
